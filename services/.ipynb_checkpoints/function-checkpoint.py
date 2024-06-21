163c308ce6ad12c20e30c1d81a3251d201b00b08
jeancarlos <jeancc.costa@gmail.com>
2024-06-21 18:39:49 -0300

    reorganizado

--- services/.ipynb_checkpoints/function-checkpoint.py
+++ services/.ipynb_checkpoints/function-checkpoint.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import re
+from datetime import date
 
-class Function():
+class Function:
     def substituir_palavras(df, palavras_a_substituir, palavra_substituta):
         # Para cada palavra a ser substituída
         for palavra in palavras_a_substituir:
@@ -11,4 +12,9 @@ class Function():
     
         # Retorna o novo DataFrame com as palavras substituídas
         return df
-
+    
+    def calculate_age(row):
+        birth_year = int(row["idade"].split("-")[0])  # Extrai o ano de nascimento a partir da data de nascimento
+        exam_year = row["ano"]  # Obtém o ano da realização da prova
+        age = exam_year - birth_year
+        return age
